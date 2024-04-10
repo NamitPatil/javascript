9548a86cddd09392616c415c683f8277aa96abb4
NamitPatil <109448523+NamitPatil@users.noreply.github.com>
2024-04-10 09:45:37 +0000

    conversion of datatypes

---
+++ basics/3_conversion.js
@@ -0,0 +1,27 @@
+let score="namit"
+
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" =33
+//"33abc"=NaN
+//true =1;false=0
+
+let isLoggedIn="namit"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 = true ;0=false
+//""=false
+//"namit"=true
+
+let somenum=33
+
+let stringnum=String(somenum)
+console.log(stringnum)
+console.log(typeof stringnum)
