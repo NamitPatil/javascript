6a09260252363b221cdeb9d9aa1095a1c6b22ebf
NamitPatil <109448523+NamitPatil@users.noreply.github.com>
2024-07-23 14:36:07 +0000

    major loops

---
+++ iteretors_5/loops.js
@@ -0,0 +1,6 @@
+//for
+var a=[1,2,3,4,5,6,7,8,9,10];
+for (let i = 0; i < a.length; i++) {
+    //const element = array[i];
+    console.log(a[i])
+}
---
+++ iteretors_5/while.js
@@ -0,0 +1,21 @@
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index++;
+    
+}
+let myArr = ['namit','sumit','vinit','sanchit'];
+let i=0;
+while (i < myArr.length) {
+    console.log(myArr[i]);
+    i++;
+}
+
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
